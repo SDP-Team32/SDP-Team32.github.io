d217021f97716858c23dd005f5675fef2db3c380
Angela Nayiga <angelanayiga@Angelas-MBP.local.tld>
2020-11-19 21:22:55 +0300

    first commit

---
+++ php/trialform-process.php
@@ -0,0 +1,58 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["occupation"])) {
+    $errorMSG = "Occupation is required ";
+} else {
+    $occupation = $_POST["occupation"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "yourname@domain.com";
+$Subject = "New trial request from Revo landing page";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Occupation: ";
+$Body .= $occupation;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
